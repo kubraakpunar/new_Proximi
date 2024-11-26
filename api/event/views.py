589b2577a036f30d9854fbc5f0e2d1553975bcfe
kubraakpunar <kubraakpunarb@gmail.com>
2024-11-26 09:28:13 +0300

    fix: delete permissions for event

--- api/event/views.py
+++ api/event/views.py
@@ -12,24 +12,19 @@ from services.event_service.models import(
     EventSchedule
 )
 from django.conf import settings 
-from rest_framework.permissions import IsAuthenticated 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated] 
 
 class EventScheduleViewSet(viewsets.ModelViewSet):
     queryset = EventSchedule.objects.all()
     serializer_class = EventScheduleSerializer
-    permission_classes = [IsAuthenticated]
 
 class EventLocationViewSet(viewsets.ModelViewSet):
     queryset = EventLocation.objects.all()
     serializer_class = EventLocationSerializer 
-    permission_classes = [IsAuthenticated]
 
 class EventRatingViewSet(viewsets.ModelViewSet):
     queryset = EventRating.objects.all()
     serializer_class = EventRatingSerializer
-    permission_classes = [IsAuthenticated]
